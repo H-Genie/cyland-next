617c6aa4354745e8e337f51436a5c04ececc9640
h-genie <ses2000hj@hanmail.net>
2026-02-22 22:54:05 +0900

    fix : vercel deploy v1

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 /**
  * Vercel 배포 시 next-swagger-doc(및 Next 빌드) 관련 ENOENT 방지.
- * .next/export-detail.json 이 없을 때 발생하는 오류를 막기 위해 생성.
+ * next build 전에 .next/export-detail.json 을 미리 생성해 두어야 빌드가 성공함.
+ * (build 스크립트에서 이 스크립트를 next build 보다 먼저 실행함)
  * @see https://github.com/jellydn/next-swagger-doc/issues/1157
  */
 const exportDetailPath = path.join(process.cwd(), ".next", "export-detail.json");
